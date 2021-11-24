1c992de464b451ed5ad5fd075ffdf90e2fbbdbc8
ta28ma2 <ta28ma2@gmail.com>
2021-11-25 00:04:29 +0900

    feat: add v-select

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,20 +1,40 @@
 <template>
+    <v-container>
+        <v-row align="center">
+            <v-col class="d-flex" cols="12" sm="6">
+                <v-select :items="category" label="Category" dense solo></v-select>
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="6">
+                <v-select :items="sort" label="Sort by:" dense solo></v-select>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
     name: 'Top',
-    components: {
-    },
+    components: {},
     data() {
         return {
+            category: [
+                'Tesla',
+                'Porsche',
+                'Toyota',
+                'Honda',
+                'Mazda',
+                'Mercedes-Benz',
+                'Lexus',
+                'Lamborghini',
+                'Audi',
+                'BMW',
+            ],
+            sort: ['Price: Low to High', 'Price: High to Low', 'Newest Arrivals'],
         };
     },
-    methods: {
-    },
-    computed: {
-    },
+    methods: {},
+    computed: {},
 });
 </script>
 
