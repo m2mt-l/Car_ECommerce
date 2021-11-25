bcd312125de100f83474d2451eec69aead63d606
ta28ma2 <ta28ma2@gmail.com>
2021-11-25 16:28:23 +0900

    feat: create Items-card.vue

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,12 +1,39 @@
-<template></template>
+<template>
+    <v-container>
+        <v-row align="center">
+            <v-col class="d-flex" cols="12" sm="6">
+                <v-select :items="category" label="Category" dense solo></v-select>
+            </v-col>
+            <v-col class="d-flex" cols="12" sm="6">
+                <v-select :items="sort" label="Sort by:" dense solo></v-select>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
 
 <script lang="ts">
 import Vue from 'vue';
+import ItemsCard from '../components/Items-card.vue'
+
 export default Vue.extend({
     name: 'Top',
     components: {},
     data() {
-        return {};
+        return {
+            category: [
+                'Tesla',
+                'Porsche',
+                'Toyota',
+                'Honda',
+                'Mazda',
+                'Mercedes-Benz',
+                'Lexus',
+                'Lamborghini',
+                'Audi',
+                'BMW',
+            ],
+            sort: ['Price: Low to High', 'Price: High to Low', 'Newest Arrivals'],
+        };
     },
     methods: {},
     computed: {},
